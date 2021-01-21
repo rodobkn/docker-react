00941c81d1115599b6c0b9c6c0526432ae6ec5b2
rodobkn <rrmendoza@uc.cl>
2021-01-21 20:57:22 -0300

    Second try

--- src/App.test.js
+++ src/App.test.js
@@ -7,7 +7,7 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders learn react link', () => {
+test('renders learn react link 2', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
